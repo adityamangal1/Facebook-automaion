94ce023e3955d228310053ac9aa4329f15e3abda
aditya mangal <adityamangal0202@gmail.com>
2021-02-11 22:54:10 +0530

    facebook automater

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from selenium import webdriver
 from time import sleep
 from webdriver_manager import driver
@@ -47,53 +46,3 @@ if user is "y":
     comment(user)
 else:
     exit()
-=======
-from selenium import webdriver
-from time import sleep
-from webdriver_manager import driver
-from webdriver_manager.chrome import ChromeDriverManager
-import pyautogui
-
-
-def login():
-    user_ID = input("Enter your email id: ")
-    user_PWD = input("Enter the password: ")
-
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get("https://www.facebook.com/")
-
-    username_box = driver.find_element_by_name('email')  # matching email id
-    username_box.send_keys(user_ID)
-    print('Sending email id...')
-
-    sleep(1)
-
-    userPass = driver.find_element_by_name('pass')  # matching password
-    userPass.send_keys(user_PWD)
-    print('Sending email password...')
-
-    sleep(1)
-    # loggin button through inspect element
-    loginnow = driver.find_element_by_id('u_0_b')
-    loginnow.submit()
-
-    print('Looged in')
-    sleep(1)
-    # exit()
-
-
-def comment(userinput):
-    # userinput = "Comenting jsut for fun!"
-    pyautogui.typewrite(userinput)
-    pyautogui.typewrite("/n")
-    print("Done")
-
-
-login()
-user = input("Press Y to comment on random post or N to exit.")
-if user is "y":
-    user = input("Type your comment: ")
-    comment(user)
-else:
-    exit()
->>>>>>> bcf3ddd527678ecfa8d61b05c4a89a2b5a4ea5a0
